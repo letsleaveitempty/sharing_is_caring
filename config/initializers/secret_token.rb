02dc38a760382262172771ed6b9b5539a8cc1f18
Anita Jaszewska <anita.jaszewska@wimdu.com>
2013-10-12 14:06:43 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharingIsCaring::Application.config.secret_token = '8565f22bad5817939cdcdeeaf8203c722c1a3c5336edb913ad5476d43d820f0c57583f0df7e059b875aaf26b3a7591658e9179b38e6b5393ec23195452112cf8'
